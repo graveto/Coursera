0b3da64c262f7ccfd8bbe96da83315d9baf17173
David Fuka <gravetotkd@gmail.com>
2015-08-02 21:26:36 -0700

    Assignment One

--- Introduction_to_Mobile_Application_Development_Using_Android/TimeGreetFriend/TimeGreetFriend/app/src/main/java/hk/ust/cse/comp107x/timegreetfriend/MainActivity.java
+++ Introduction_to_Mobile_Application_Development_Using_Android/TimeGreetFriend/TimeGreetFriend/app/src/main/java/hk/ust/cse/comp107x/timegreetfriend/MainActivity.java
@@ -1,4 +1,4 @@
-package hk.ust.cse.comp107x.greetfriend;
+package hk.ust.cse.comp107x.timegreetfriend;
 
 import android.app.Activity;
 import android.os.Bundle;
@@ -17,13 +17,16 @@ public class MainActivity extends Activity implements View.OnClickListener {
 
     Button greetButton;
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // get a reference to the greetButton on the UI
         greetButton = (Button) findViewById(R.id.greetButton);
+        // Set the onClickListener for the greetButton to be this class.
+        // This requires that the class implement the View.OnClickListener callback
+        // the onClick() method
         greetButton.setOnClickListener(this);
     }
 
@@ -52,10 +55,19 @@ public class MainActivity extends Activity implements View.OnClickListener {
     @Override
     public void onClick(View v) {
 
+        // get a reference to the TextView on the UI
         TextView textMessage = (TextView) findViewById(R.id.textMessage);
+
+        //get a reference to the EditText so that we can read in the value typed
+        // by the user
         EditText editFriendName = (EditText) findViewById(R.id.editFriendName);
+
+        // get the name of the friend typed in by the user in the EditText field
         String friendName = editFriendName.getText().toString();
 
+        // TODO 1
+        // Add the code to display the appropriate greeting message. Your output message should be exactly the same as the one you see on the video with exactly the same number of spaces in the same positions.
+        // The code from the previous exercise is commented and kept below for your reference
         switch (v.getId()) {
             case R.id.greetButton:
                 textMessage.setText(getGreeting(getHourOfDay()) + friendName + "!");
